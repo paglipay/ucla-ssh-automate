9c70cc40d85efb17fd59ee3a56a07b389d1fd911
paglipay <paglipay@gmail.com>
2022-12-09 00:40:22 +0000

    jinja works

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import DTree
 import json
-# import sys
 import pprint as pp
 from flask import Flask, jsonify, request, send_file
-# from flask_socketio import SocketIO, emit
-# from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# from threading import Thread
 import logging
 import threading
 import time
@@ -43,22 +39,18 @@ def do_process(flask_data, json_file, import_obj_instance):
 
 @app.route('/start/<hash>', methods=['GET', 'POST'])
 def start(hash):
-    # name = names[hash]
     name = hash
     d = {}
     if request.method == 'POST':
         data = request.get_json()
-        print('data: ', data)
         if 'jobs' in data:
             name = data.pop('jobs')
         d = data
-    print('d: ', d)
     if hash in flask_process_data:
         flask_process_data[hash].update(d)
         import_obj_instance = import_obj_instance_hash[hash]
 
     else:
-        print('NEW HASH: ', hash)
         flask_process_data[hash] = d
         import_obj_instance_hash[hash] = {}
         import_obj_instance = import_obj_instance_hash[hash]
@@ -74,6 +66,11 @@ def start(hash):
     d['uuid'] = hash
 
     return jsonify(d), 200
+
+@app.route('/show/<hash>', methods=['GET', 'POST'])
+def show(hash):
+    flask_data_temp = flask_process_data[hash]
+    return jsonify(flask_data_temp), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

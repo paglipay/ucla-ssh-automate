0973e03af537b62940af6fea4149e44c02c28e24
paglipay <paglipay@gmail.com>
2022-12-09 00:45:30 +0000

    './jobs/jinja_test/list.json'

--- tests/Jinja_test.py
+++ tests/Jinja_test.py
@@ -4,18 +4,21 @@ import json
 
 import_obj_instance_hash = {}
 
+
 class Testing(unittest.TestCase):
 
-    def test_key(self):
+    def test_jinja(self):
 
-        json_file = './jobs/jinja_test/jinja_test.json'
+        json_file = "./jobs/jinja_test/jinja_test.json"
         flask_data = {}
         import_obj_instance = {}
         c = DTree.DTree(json.load(open(json_file)), name=json_file,
                         import_obj_instance=import_obj_instance, data=flask_data)
 
-        # print(c.data)
-        assert c.data == {'Key': ['Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD', 'GOOD', 'Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD', 'GOOD']}
+        print('c.data: ', c.data)
+        assert c.data == {
+            './jobs/jinja_test/list.json': ['R1', 'R2', 'R3', 'R4', 'R5']}
+
 
 if __name__ == '__main__':
     unittest.main()

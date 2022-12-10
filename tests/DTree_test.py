b9583139cc9844c354bffcdef4426fb42ed5e522
Paul <paglipay@gmail.com>
2022-12-09 16:00:23 -0800

    form_dic added

--- tests/DTree_test.py
+++ tests/DTree_test.py
@@ -4,6 +4,7 @@ import json
 
 import_obj_instance_hash = {}
 
+
 class Testing(unittest.TestCase):
 
     def test_key(self):
@@ -15,7 +16,21 @@ class Testing(unittest.TestCase):
                         import_obj_instance=import_obj_instance, data=flask_data)
 
         # print(c.data)
-        assert c.data == {'Key': ['Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD', 'GOOD', 'Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD', 'GOOD']}
+        assert c.data == {'Key': ['Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD',
+                                  'GOOD', 'Hello', 'World', 'BAD', 'GOOD', '3BAD', '3GOOD', 'GOOD']}
+
+    def test_jinja_open(self):
+
+        json_file = './jobs/open/_create_list.json'
+        flask_data = {}
+        import_obj_instance = {}
+        c = DTree.DTree(json.load(open(json_file)), name=json_file,
+                        import_obj_instance=import_obj_instance, data=flask_data)
+
+        print(c.data)
+        assert c.data == {'./jobs/open/form_dic.json': [{'ip': ['ip'], 'username': ['username'], 'password': [
+            'password']}], './jobs/open/do.json': [{'open': {'ip': 'ip', 'username': 'username', 'password': 'password'}}]}
+
 
 if __name__ == '__main__':
     unittest.main()
